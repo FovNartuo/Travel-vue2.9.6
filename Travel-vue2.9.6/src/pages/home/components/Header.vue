4d935cef88e0040f7a3bb6644b32e693a629b4de
FovNartuo <2205446031@qq.com>
2019-03-11 21:16:40 +0800

    add header

---
+++ Travel-vue2.9.6/src/pages/home/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      返回
+    </div>
+    <div class="header-input">
+      输入城市
+    </div>
+    <div class="header-right">
+      城市
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<!--scoped保证只对这个组件影响样式-->
+<style lang="stylus" scoped>
+</style>
