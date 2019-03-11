54b6ba3fa4f3d60da40c0171b431de3b09c54b0c
FovNartuo <2205446031@qq.com>
2019-03-11 22:00:27 +0800

    change recommend

---
+++ Travel-vue2.9.6/src/pages/home/components/Weekend.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item" v-for="item of weekendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" >
+        </div>
+        <div class="item-info">
+          <p class="item-title" v-text="item.title"></p>
+          <p class="item-desc" v-text="item.desc"></p>
+        </div>
+
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/ed/bf2bc72af591d5.jpg_r_640x214_f9f1bfcc.jpg',
+        title: '温泉必游',
+        desc: '为你量身打造绿色健康生活方式，远离都市的喧嚣，工作的烦恼，还你一个悠闲静雅之旅'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/f1/ae9d4299b8aaea.jpg_r_640x214_0c555905.jpg',
+        title: '南京必打卡',
+        desc: '看尽王侯将相荣辱成败，六朝金粉潮起潮落'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/e1/cdbf56d837da59.jpg_r_640x214_105fc3f2.jpg',
+        title: '民国情怀国共风华',
+        desc: '南京，一座历史文化名城，这片土地上曾经经历过太多太多值得我们去追忆的往事'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 37.4375%
+    .item-img
+      width: 100%
+  .item-img
+    width: 1.8rem
+    height: 1.8rem
+    padding: .1rem
+  .item-info
+    flex: 1
+    padding: .1rem
+    min-width 0
+    min-width: 0
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+    .item-button
+      line-height: .44rem
+      margin-top: .2rem
+      background: #ff9300
+      padding: 0 .2rem
+      border-radius: .06rem
+      color:#fff
+</style>
