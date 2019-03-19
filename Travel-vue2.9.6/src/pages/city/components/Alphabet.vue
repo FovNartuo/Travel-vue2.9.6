d6fa8f14eaeaf9e5ee63db4e927e574a99b5efff
FovNartuo <2205446031@qq.com>
2019-03-19 21:23:49 +0800

    perfomance

--- Travel-vue2.9.6/src/pages/city/components/Alphabet.vue
+++ Travel-vue2.9.6/src/pages/city/components/Alphabet.vue
@@ -1,6 +1,17 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
+    <li
+      class="item"
+      v-for="item of letter"
+      :key="item"
+      :ref="item"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      @click="handleLetterClick"
+    >
+      {{item}}
+    </li>
   </ul>
 </template>
 
@@ -9,6 +20,53 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      // 函数节流
+      timer: null
+    }
+  },
+  updated () {
+    // A距离蓝色框下沿的距离
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  computed: {
+    letter () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          // 手指触点到蓝色框下沿的距离
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 22)
+          if (index >= 0 && index < this.letter.length) {
+            this.$emit('change', this.letter[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
